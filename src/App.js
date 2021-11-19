9b343f66be48d847f9b8a5de4df21406e84cd328
Veronchi <zhigunova1995@gmail.com>
2021-11-19 18:13:17 +0300

    created an initial state of the birthday list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [persons, setPersons] = useState(data);
+
+  return (
+    <main>
+      <List personsList={persons}/>
+    </main>
+  );
+
 }
+
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ personsList }) => {
+  console.log(personsList)
+
+  const person = personsList.map(({ id, name, age, image }) => {
+    return (
+      <li key={id} className='person'>
+        <img src={image} alt='person' />
+        <div>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+        </div>
+      </li>
+    );
+  })
+
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <div className='container'>
+      <h3>{personsList.length} birthdays today</h3>
+      <ul>
+        {person}
+      </ul>
+      <button>Clear All</button>
+    </div>
   );
 };
 
